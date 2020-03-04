d2ba0e3a26fd9e8c5c43e275150e4ef6258cf4f8
Marc <marc.maninang@gmail.com>
2020-03-03 18:52:34 -0800

    Init project
    Moved code from codepen to here.  Reimport to codepen when done.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# js-calculator
- JavaScript powered calculator
+js-calculator
+# JavaScript powered calculator
+
+## TODO
+Functions for each operation: + , - , x , /
+  
+_NOTE:_ Do NOT create button event listeners for each function
---
+++ script.js
@@ -0,0 +1,26 @@
+// set all buttons as variable
+const buttons = document.querySelectorAll('button');
+
+// set calculation screen
+let calScreen = document.getElementById('calculation');
+
+// listen for when any button is pressed
+let calInput = buttons.forEach(button => {
+  button.addEventListener("click", function () {
+    collectInput(this.value);
+  });
+});
+
+// this is where we take the input from the calculator and do something with it
+function collectInput(input) {
+  switch (input) {
+
+    // clear all current inputs
+    case 'clear':
+      console.log('You hit the Clear button');
+      break;
+    case '=':
+      console.log('You hit the equals button');
+      break;
+  }
+}
