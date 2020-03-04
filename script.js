6ab0209fc8004064dc7c63ecf1cacfaac13f3ae8
Marc <marc.maninang@gmail.com>
2020-03-03 20:33:51 -0800

    properly capture user input
    also removed operator functions for now.

--- script.js
+++ script.js
@@ -4,36 +4,21 @@ const buttons = document.querySelectorAll('button');
 // set calculation screen
 let calScreen = document.getElementById('calculation');
 
+let currInput = '';
+
 // listen for when any button is pressed
 let input = buttons.forEach(button => {
   button.addEventListener("click", function () {
 
     // collect user input
-    let currInput = this.value;
-    
+    currInput += this.value;
+
     // output to screen
-    calScreen.innerHTML += currInput;
-
-    // do something with the input
-    if ( currInput === '+' ) {
-      add(currInput);
-    }
-
+    calScreen.innerHTML = currInput;
+    
   });
 });
 
-function add(num) {
-  return num + num;
-}
+function operate() {
 
-function subctract(num) {
-  return num - num;
-}
-
-function multiple(num) {
-  return num * num;
-}
-
-function divide(num) {
-  return num / num;
 }
